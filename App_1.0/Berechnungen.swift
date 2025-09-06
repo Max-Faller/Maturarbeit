48207ea62c6ad665ff6a0a53cbc46d4eeb117a97
Max-Faller <max_faller@icloud.com>
2025-09-06 17:53:22 +0200

    Beschleunigungssensoren auslesen und ausgeben 1.0

---
+++ App_1.0/Berechnungen.swift
@@ -0,0 +1,47 @@
+//
+//  App_1_0App.swift
+//  App_1.0
+//
+//  Created by Max Faller on 30.08.2025.
+//
+
+import SwiftUI
+import CoreMotion
+
+/*
+
+import Foundation
+
+class SharedState {
+    
+//Variabeln geteilt für Contentview und Berechnungen
+    
+    @Published var start = false
+    
+}
+
+if SharedState(start) == true {
+    print("hallo")
+}
+
+init() {
+    let state = SharedState()
+    self.sharedState = state
+    self.calculator = Calculator(sharedState: state)
+}
+
+*/
+
+
+
+
+@main
+struct Berechnungen: App {
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- App_1.0/ContentView.swift
+++ App_1.0/ContentView.swift
@@ -5,32 +5,84 @@
 //
 import MapKit
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
     
+// Variabeln:
+    
     @State private var Textfeld = false
     @State private var visibleButton = true
-    var text = "Hallo"
+    @State var text = "Beschleunigung:"
+    @State var start = false
+    
+//Beschneunigung Variablen:
+    
+    let manager = CMMotionManager()
+    @State var xBeschleunigung = 0.00000
+    @State var yBeschleunigung = 0.00000
+    @State var zBeschleunigung = 0.00000
+    @State var timer: Timer?
+    
+
+    
     
     var body: some View {
- 
-        if visibleButton == true {
-            Button {
-                visibleButton = false
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    Textfeld = true
+        VStack {
+            if visibleButton == true {
+                Button {
+                    visibleButton = false
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        Textfeld = true
+                        start = true
+                    }
+                } label: {
+                    Image(systemName: "play.square")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 60)
                 }
-            } label: {
-                Image(systemName: "play.square")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(height: 60)
+            }
+            
+            if Textfeld == true {
+                Text(text)
+                    .padding()
+                    .font(.system(size: 30))
+                Text("X: \(xBeschleunigung, specifier: "%.5f")")
+                    .padding(.top, 5)
+                Text("Y: \(yBeschleunigung, specifier: "%.5f")")
+                    .padding(.top, 5)
+                Text("Z: \(zBeschleunigung, specifier: "%.5f")")
+                    .padding(.top, 5)
             }
         }
-        if Textfeld == true {
-            Text(text)
-                .padding()
-                .font(.system(size: 30))
+        .onChange(of: start) { v in
+                    if v == true {
+                        startAccelerometerUpdates()
+                    }
+                }
+        .onDisappear {
+                manager.stopAccelerometerUpdates()
+                timer?.invalidate()
+                timer = nil
+        }
+    }
+        
+    private func startAccelerometerUpdates() {
+        guard manager.isAccelerometerAvailable else {
+            return
+            }
+        
+        manager.startAccelerometerUpdates()
+        manager.accelerometerUpdateInterval = 0.01
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { timer in
+            if let Beschleunigung = self.manager.accelerometerData {
+                xBeschleunigung = Beschleunigung.acceleration.x
+                yBeschleunigung = Beschleunigung.acceleration.y
+                zBeschleunigung = Beschleunigung.acceleration.z
+                
+            }
         }
     }
 }
