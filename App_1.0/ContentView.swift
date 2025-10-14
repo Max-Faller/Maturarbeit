8841d8c494e41b8205b82ab2aa88c4a6f2360e25
Max-Faller <max_faller@icloud.com>
2025-10-14 18:39:01 +0200

    Stand 13.Okt und nach 30min arbeiten 14 Okt.

--- App_1.0/ContentView.swift
+++ App_1.0/ContentView.swift
@@ -6,35 +6,81 @@
 import MapKit
 import SwiftUI
 import CoreMotion
+import CoreLocation
+
+class LocationDelegate: NSObject, CLLocationManagerDelegate {
+    var onLocationUpdate: ((Double, Double, Double) -> Void)?
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            onLocationUpdate?(location.coordinate.latitude, location.coordinate.longitude, location.altitude)
+        }
+    }
+}
 
 struct ContentView: View {
     
-// Variabeln:
+    // Variabeln:
     
     @State private var Textfeld = false
     @State private var visibleButton = true
     @State var text = "Beschleunigung:"
     @State var start = false
     
-//Beschneunigung Variablen:
+    //Beschneunigung Variablen:
     
     let manager = CMMotionManager()
     
-    @State var xBeschleunigung = 0.00000
-    @State var yBeschleunigung = 0.00000
-    @State var zBeschleunigung = 0.00000
-    @State var maxBeschleunigung = 0.00000
+    @State var xBeschleunigung = 0.00
+    @State var yBeschleunigung = 0.00
+    @State var zBeschleunigung = 0.00
+    @State var xBeschleunigungTotal = 0.00
+    @State var yBeschleunigungTotal = 0.00
+    @State var zBeschleunigungTotal = 0.00
+    @State var maxBeschleunigung = 0.00
     
-//Geschwindikeit Variabeln:
+    @State var Zaeler = 0
     
-    @State var xGeschwindigkeit = 0.00000
-    @State var yGeschwindigkeit = 0.00000
-    @State var zGeschwindigkeit = 0.00000
-    @State var gesammtGeschwindigkeit = 0.00000
+    //Geschwindikeit Variabeln:
+    
+    @State var xGeschwindigkeit = 0.00
+    @State var yGeschwindigkeit = 0.00
+    @State var zGeschwindigkeit = 0.00
+    @State var gesammtGeschwindigkeit = 0.00
+    
+    @State var xDistanz = 0.00
+    @State var yDistanz = 0.00
+    @State var zDistanz = 0.00
+    @State var gesammtDistanz = 0.00
     
     @State var timer: Timer?
-
-//UI:
+    
+    //GPS Variabeln:
+    
+    @State private var locationManager = CLLocationManager()
+    @State private var locationDelegate = LocationDelegate()
+    @State var BreitenGrad = 0.00
+    @State var LaengenGrad = 0.00
+    @State var altBreitenGrad = 0.00
+    @State var altLaengenGrad = 0.00
+    @State var Location = false
+    
+    @State var DeltaBG = 0.00
+    @State var DeltaLG = 0.00
+    @State var MeterBewegungBG = 0.00
+    @State var MeterBewegungLG = 0.00
+    
+    @State var hoehe = 0.00 // in m.ü.M
+    @State var althoehe = 0.00 // in m.ü.M
+    @State var DeltaHoehe = 0.00
+    
+    @State var DeltaBewegung = 0.00
+    @State var gesammtBewegung = 0.00
+    
+    @State var einmalig = 0
+    
+    
+    //UI:
     
     var body: some View {
         VStack {
@@ -57,37 +103,67 @@ struct ContentView: View {
                 Text(text)
                     .padding()
                     .font(.system(size: 30))
-                Text("X: \(xBeschleunigung, specifier: "%.5f")")
+                Text("X: \(xBeschleunigung, specifier: "%.3f")")
                     .padding(.top, 5)
-                Text("Y: \(yBeschleunigung, specifier: "%.5f")")
+                Text("Y: \(yBeschleunigung, specifier: "%.3f")")
                     .padding(.top, 5)
-                Text("Z: \(zBeschleunigung, specifier: "%.5f")")
+                Text("Z: \(zBeschleunigung, specifier: "%.3f")")
                     .padding(.top, 5)
-                //Text("Max:\(maxBeschleunigung, specifier: "%.5f")")
-                Text("X: \(xGeschwindigkeit, specifier: "%.5f")")
+                //Text("Max:\(maxBeschleunigung, specifier: "%.3f")")
+                Text("X: \(xDistanz, specifier: "%.3f")")
                     .padding(.top, 5)
-                Text("Y: \(yGeschwindigkeit, specifier: "%.5f")")
+                Text("Y: \(yDistanz, specifier: "%.3f")")
                     .padding(.top, 5)
-                Text("Z: \(zGeschwindigkeit, specifier: "%.5f")")
+                Text("Z: \(zDistanz, specifier: "%.3f")")
                     .padding(.top, 5)
-                Text("Geschwindgkeit:\(gesammtGeschwindigkeit, specifier: "%.5f")")
+                Text("Geschwindgkeit:\(gesammtGeschwindigkeit, specifier: "%.3f")")
+                    .padding(.top, 5)
+                Text("Distanz:\(gesammtDistanz, specifier: "%.3f")")
+                    .padding(.top, 5)
+                Text("Längengrad: \(LaengenGrad, specifier: "%.3f")")
+                    .padding(.top, 5)
+                Text("Breitengrad: \(BreitenGrad, specifier: "%.3f")")
+                    .padding(.top, 5)
+                Text("Höhe über Meer: \(hoehe, specifier: "%.3f")")
+                    .padding(.top, 5)
                 
             }
         }
+        
+        
+        
+        
+        
+        .onAppear {
+            locationDelegate.onLocationUpdate = { lat, lon, alt in
+                BreitenGrad = lat
+                LaengenGrad = lon
+                hoehe = alt
+            }
+            
+            locationManager.delegate = locationDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
         .onChange(of: start) { newValue in
             if newValue == true {
-                print(true)
                 startMessung()
                 GeschwindigkeitsTimer()
-                        
             }
         }
+        
         .onDisappear {
             manager.stopDeviceMotionUpdates()
             timer?.invalidate()
             timer = nil
+            locationManager.stopUpdatingLocation()
         }
     }
+    
+    
+    //Funktionen
     
     
     private func startMessung() {
@@ -99,7 +175,7 @@ struct ContentView: View {
         }
         
         //INtervall
-        manager.deviceMotionUpdateInterval = 0.01
+        manager.deviceMotionUpdateInterval = 0.02
         
         // Start Messung
         manager.startDeviceMotionUpdates(to: .main) { motion, error in
@@ -110,28 +186,84 @@ struct ContentView: View {
                 yBeschleunigung = acc.y
                 zBeschleunigung = acc.z
                 
-                //Ueberpruefen ob es richtig funktionniert
-                if maxBeschleunigung < abs(acc.x) {
-                    maxBeschleunigung = abs(acc.x)
-                }
-                if maxBeschleunigung < abs(acc.z) {
-                    maxBeschleunigung = abs(acc.z)
-                }
-                if maxBeschleunigung < abs(acc.y) {
-                    maxBeschleunigung = abs(acc.y)
-                }
+                xBeschleunigung = round(xBeschleunigung * 1000) / 1000
+                yBeschleunigung = round(yBeschleunigung * 1000) / 1000
+                zBeschleunigung = round(zBeschleunigung * 1000) / 1000
+            }
+            
+            //Sensorrauschen im Ruhezustand
+            if xBeschleunigung < 0.01 && xBeschleunigung > -0.01 {
+                xBeschleunigung = 0.00000
+            }
+            if yBeschleunigung < 0.01 && yBeschleunigung > -0.01 {
+                yBeschleunigung = 0.00000
+            }
+            
+            if zBeschleunigung < 0.01 && zBeschleunigung > -0.01 {
+                zBeschleunigung = 0.00000
             }
         }
     }
     
     func GeschwindigkeitsTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
-            // Integriere X,Y,Z
-            xGeschwindigkeit = xGeschwindigkeit + xBeschleunigung * 9.81 * 0.01
-            yGeschwindigkeit = yGeschwindigkeit + yBeschleunigung * 9.81 * 0.01
-            zGeschwindigkeit = zGeschwindigkeit + zBeschleunigung * 9.81 * 0.01
+        timer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { _ in
             
-            gesammtGeschwindigkeit = sqrt(pow(xGeschwindigkeit, 2) + pow(yGeschwindigkeit, 2) + pow(zGeschwindigkeit, 2))
+            //xBeschleunigungTotal = xBeschleunigungTotal + xBeschleunigung
+            //yBeschleunigungTotal = yBeschleunigungTotal + yBeschleunigung
+            //zBeschleunigungTotal = zBeschleunigungTotal + zBeschleunigung
+            
+            //Zaeler = Zaeler + 1
+            
+            //if Zaeler == 20 {
+                //Zaeler = 0
+            
+                // Integriere X,Y,Z
+            xGeschwindigkeit = xGeschwindigkeit + xBeschleunigung * 9.81 * 0.02
+            yGeschwindigkeit = yGeschwindigkeit + yBeschleunigung * 9.81 * 0.02
+            zGeschwindigkeit = zGeschwindigkeit + zBeschleunigung * 9.81 * 0.02
+ 
+            gesammtGeschwindigkeit = sqrt((xGeschwindigkeit * xGeschwindigkeit) + (yGeschwindigkeit * yGeschwindigkeit) + (zGeschwindigkeit * zGeschwindigkeit))
+
+                // Integriere X,Y,Z
+            xDistanz = xDistanz + xGeschwindigkeit * 0.02
+            yDistanz = yDistanz + yGeschwindigkeit * 0.02
+            zDistanz = zDistanz + zGeschwindigkeit * 0.02
+
+            gesammtDistanz = sqrt((xDistanz * xDistanz) + (yDistanz * yDistanz) + (zDistanz * zDistanz))
+
+            
+                //xBeschleunigungTotal = 0.00000
+                //yBeschleunigungTotal = 0.00000
+                //zBeschleunigungTotal = 0.00000
+            
+        }
+    }
+    
+    func GPSberechnung() {
+        timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+        
+            if einmalig == 0 {  //beim ersten Durchgang wird keine Bewegung registriert
+                einmalig = 1
+                altBreitenGrad = BreitenGrad
+                altLaengenGrad = LaengenGrad
+                althoehe = hoehe
+            }
+            
+            DeltaBG = BreitenGrad - altBreitenGrad //111.133m pro Breitengrad veränderung
+            DeltaLG = LaengenGrad - altLaengenGrad //111.319m * cos(BreitenGrad) pro Längengrad veränderung
+            DeltaHoehe = hoehe - althoehe
+            
+            MeterBewegungBG = abs(DeltaBG) * 111.133
+            MeterBewegungLG = abs(DeltaLG) * 111.319 * cos(BreitenGrad)
+            
+            altBreitenGrad = BreitenGrad
+            altLaengenGrad = LaengenGrad
+            althoehe = hoehe
+            
+            //Strecke in Metern
+            
+            DeltaBewegung = sqrt((MeterBewegungBG * MeterBewegungBG) + (MeterBewegungLG * MeterBewegungLG) + (DeltaHoehe * DeltaHoehe)) // veränderung
+            gesammtBewegung = gesammtBewegung + DeltaBewegung    // gesammt Strecke
             
         }
     }
@@ -140,3 +272,9 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
+
+
+
